7007f093dc099a787d4c9511101c43821511c880
tanaphatkaewsri <tanaphatkaewsri@gmail.com>
2023-03-30 11:18:29 +0700

    check point for 1 hr
    edit more for add more code and folder

--- app.js
+++ app.js
@@ -5,23 +5,27 @@ const morgan = require('morgan');
 const path = require('path');
 
 const app = express(); // call Express run app
-const port = 3000; // set port
+const PORT = process.env.PORT || 4000; // set port
 
 app.use(morgan('combined')); // use morgan
 // show request?, windows?, os? information
 
 app.use(express.static(path.join(__dirname, "/public/"))) // for use path that we specific -> tell where is static path -> directory path for tell where is express -> and use folder that keep static file
 
+app.set("views", "./src/views"); // "./ is for tell that we want to access the folder"
+app.set("view engine", "ejs"); // use view engine and call ejs for working
+
 app.get("/", (req, res) => { // for manage request that comming at path "/", if they come in this page we will response something
-    res.send('Hello World'); // we will response txt 'Hello World'
+    // res.send('Hello World'); // we will response txt 'Hello World'
+    res.render('index', {username: "Young Lady", customer: ["Kitti", "Tanaphat", "YoungLady"]}); // when we use ejs we will use .render instead .sent // render to index.ejs and {data that we want to sent to ejs} --> for use dynamic web , and we can add not only str but array too
 })
 
 // app.listen(port, () => { // use for waitting for listen at port and do something when port is working
 //     console.log("Listening on port " + chalk.green(port)) // show output at console.log
 // })
 
-app.listen(port, () => {
-    debug("Listening on port " + chalk.green(port)) // show debug output
+app.listen(PORT, () => {
+    debug("Listening on port " + chalk.green(PORT)) // show debug output
 })
 
 // at node.js can working on computer (outside web app) so you can run js at terminal directly
